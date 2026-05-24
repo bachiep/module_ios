f4307b722f37f67eec74b12481b138a72305e519
pachip <hhkk77441@gmail.com>
2026-05-24 23:19:23 +0700

    Update Locket subscription purchase date to 2026

--- js/Locket_bachiep.js
+++ js/Locket_bachiep.js
@@ -24,14 +24,14 @@ var bachiep = {
       grace_period_expires_date: null,
       refunded_at: null,
       unsubscribe_detected_at: null,
-      original_purchase_date: "2005-01-09T10:10:15Z",
-      purchase_date: "2005-01-09T10:10:14Z",
+      original_purchase_date: "2026-01-09T10:10:15Z",
+      purchase_date: "2026-01-09T10:10:14Z",
       store: "app_store",
       store_transaction_id: "2000001108724193",
   },
   locketGold = {
       grace_period_expires_date: null,
-      purchase_date: "2005-01-09T10:10:14Z",
+      purchase_date: "2026-01-09T10:10:14Z",
       product_identifier: "locket_1600_1y",
       expires_date: "9999-01-09T10:10:14Z"
   };
